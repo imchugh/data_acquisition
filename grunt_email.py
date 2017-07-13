ddaf7540566c02b623642b5b41d8c9a2ad0bae78
imchugh <ian.mchugh@monash.edu>
2017-07-13 15:46:49 +1000

    Just cleaning up

---
+++ grunt_email.py
@@ -0,0 +1,26 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+from email.Utils import formatdate
+
+def email_send(to, subject, bodytext):
+
+    assert isinstance(to, list)
+    to_str = ','.join(to)
+    fro = 'noreply@ozflux-grunt.novalocal'
+
+    msg = MIMEMultipart()
+    msg['From'] = fro
+    msg['To'] = to_str
+    msg['Date'] = formatdate(localtime = True)
+    msg['Subject'] = subject
+    msg.attach(MIMEText(bodytext))
+
+    try:
+        smtpObj = smtplib.SMTP('localhost')
+        smtpObj.sendmail(fro, to, msg.as_string())        
+        print "Successfully sent email"
+    except SMTPException:
+        print "Error: unable to send email"
+
+    smtpObj.close()
