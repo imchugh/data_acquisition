37a82754d33ce16c54031ae08013619078cfa567
Ian McHugh <ian_mchugh@fastmail.com>
2017-08-22 13:36:11 +1000

    Should be ready for acquisition

--- fetch_access_data.py
+++ fetch_access_data.py
@@ -8,7 +8,6 @@
 import os, sys
 from subprocess import call as spc
 from datetime import date, timedelta
-import pdb
 import xlrd
 
 #####
@@ -17,7 +16,6 @@ svr = 'opendap.bom.gov.au:8080'
 pth = '/thredds/fileServer/bmrc/access-r-fc/ops/surface/'
 
 tmpfile = './temp/access.nc'
-site_list = '/rdsi/market/access_opendap/sites_list.txt'
 
 # check if date is on command line - else default to yesterday
 args = sys.argv[1:]
@@ -27,10 +25,6 @@ if len(args) < 1:
 else:
     ymd = args[0]
 
-#print ymd
-#exit(0)
-
-#ymd = '20150514'
 ym = ymd[:6]
 hrs = ['00', '06', '12', '18']
 pref = '/ACCESS-R_' 
@@ -47,36 +41,41 @@ if not os.path.exists(ym):
 #wget = '/usr/bin/wget -nv -N -nH -np -a Download.log -P ' + ym + ' -0 ' + tmpfile + ' '
 wget = '/usr/bin/wget -nv -a Download.log -O ' + tmpfile + ' '
 
-# read in site lat/lon
-with open(site_list, 'r') as fin:
-    sites = list(fin)
-pdb.set_trace()
-#####
-#for h in hrs:
-#    for t in range(7):
-#        access = pref + ymd + h + '_' + str(t).zfill(3) + suff
-#        cmd = wget + prot + svr + pth + ymd + h + access
-#        print cmd
-#        if spc(cmd, shell=True):
-#            print 'Error in command: ', cmd
-#        else:
-#            # extract data for the sites, then can throw away the tmpfile
-#        #if True:
-#            for sl in sites:
-#                sname, slat, slon = sl.split(',')
-#               sname = sname.replace(' ', '_')
-#                lat = float(slat)
-#                lon = float(slon.strip())
-#                #print sname, lat, lon
-#                ncks = '/usr/bin/ncks -d lat,' + str(lat - delta) + ',' + str(lat + delta)  + ' -d lon,' \
-#                     + str(lon - delta) + ',' + str(lon + delta)  + ' ' \
-#                     + tmpfile + ' ' + ym + '/' + sname + '_' + ymd + h + '_' + str(t).zfill(3) + '.nc'
-#                print ncks
-#                if spc(ncks, shell=True):
-#                    print 'Error in command: ', ncks
-#           # 
-#            os.remove(tmpfile)
- 
+file_path = '/home/ian/Temp/site_master.xls'
+wb = xlrd.open_workbook(file_path)
+sheet = wb.sheet_by_name('Active')
+header_row = 9
+start_row = 10
+header_list = sheet.row_values(header_row)
+site_list = sheet.col_values(header_list.index('Site'), start_row)
+lat_list = sheet.col_values(header_list.index('Latitude'), start_row)
+lat_list = [float(str(this).strip()) for this in lat_list]
+lon_list = sheet.col_values(header_list.index('Longitude'), start_row)
+lon_list = [float(str(this).strip()) for this in lon_list]
+input_list = zip(site_list, lat_list, lon_list)
 
-#print ' --- All done ---'
+#####
+for h in hrs:
+    for t in range(7):
+        access = pref + ymd + h + '_' + str(t).zfill(3) + suff
+        cmd = wget + prot + svr + pth + ymd + h + access
+        print cmd
+        if spc(cmd, shell=True):
+            print 'Error in command: ', cmd
+        else:
+            # extract data for the sites, then can throw away the tmpfile
+            for sl in input_list:
+                name = sl[0].replace(' ', '_')
+                lat = sl[1]
+                lon = sl[2]
+                #print name, lat, lon
+                ncks = '/usr/bin/ncks -d lat,' + str(lat - delta) + ',' + str(lat + delta)  + ' -d lon,' \
+                     + str(lon - delta) + ',' + str(lon + delta)  + ' ' \
+                     + tmpfile + ' ' + ym + '/' + name + '_' + ymd + h + '_' + str(t).zfill(3) + '.nc'
+                print ncks
+                if spc(ncks, shell=True):
+                    print 'Error in command: ', ncks
+           # 
+            os.remove(tmpfile)
  
+print ' --- All done ---'
