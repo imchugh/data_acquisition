99cdcc56cf3e774839bb1c7c1f0845fcebe9ef5c
imchugh <ian.mchugh@monash.edu>
2017-08-24 08:46:27 +1000

    Small changes

--- access_concatenate_daily.py
+++ access_concatenate_daily.py
@@ -528,6 +528,91 @@ def access_read_mfiles2(file_list,var_list=[]):
 
 def makedummyseries(shape):
     return numpy.ma.masked_all(shape)
+
+def makecontroldict(base_dir, yearmonth, master_file_path):
+    ### Define working and final dictionaries ###
+    
+    # ACCESS variable name alias dictionary
+    access_alias_dict = {'Fsd': 'av_swsfcdown',
+                         'Fn_sw': 'av_netswsfc',
+                         'Fld': 'av_lwsfcdown',
+                         'Fn_lw': 'av_netlwsfc',
+                         'Ta': 'temp_scrn',
+                         'q': 'qsair_scrn',
+                         'Sws': 'soil_mois',
+                         'Ts': 'soil_temp',
+                         'u': 'u10',
+                         'v': 'v10',
+                         'ps': 'sfc_pres',
+                         'Precip': 'accum_prcp',
+                         'Fh': 'sens_hflx',
+                         'Fe': 'lat_hflx',
+                         'Habl': 'abl_ht'}
+    
+    # Site and file naming alias dictionary
+    site_alias_dict = {'Emerald': {'dict_name': 'Emerald',
+                                   'site_name': 'Emerald',
+                                   'in_filename': 'Arcturus',
+                                   'out_filename': 'Emerald'},
+                       'Yanco': {'dict_name': 'Jaxa',
+                                 'site_name': 'Yanco',
+                                 'in_filename': 'Jaxa',
+                                 'out_filename': 'Yanco'}}
+    
+    # Final dictionary
+    master_dict = {'Sites': {},
+                   'Variables': {},
+                   'Options': {}}
+    
+    ### Build the 'Sites' control dicts ###
+        
+    # Get the data from the master file and initialise location list for timezone
+    wb = xlrd.open_workbook(master_file_path)
+    sheet = wb.sheet_by_name('Active')
+    site_list = [str(site.value) for site in sheet.col(0, 10)]
+    lat_list = [site.value for site in sheet.col(4, 10)]
+    lon_list = [site.value for site in sheet.col(5, 10)]
+    loc_d = {site[0]: site[1:] for site in zip(site_list, lat_list, lon_list)}
+    
+    # Iterate over sites
+    for site_name in site_list:
+           
+        this_dict = {}
+        
+        site_loc = loc_d[site_name]
+        this_dict['site_timezone'] = tzf().timezone_at(lng = site_loc[1], 
+                                                       lat = site_loc[0])
+        
+        try:
+            this_alias_dict = site_alias_dict[site_name]
+            site_name = this_alias_dict['site_name']
+            in_filename_part = this_alias_dict['in_filename'].replace(' ', '_')
+            out_filename_part = this_alias_dict['out_filename'].replace(' ', '')
+            dict_name = this_alias_dict['dict_name'].replace(' ', '')
+        except KeyError:
+            in_filename_part = site_name.replace(' ', '_')
+            out_filename_part = site_name.replace(' ', '')
+            dict_name = out_filename_part
+        this_dict['in_filename'] = '{}*.nc'.format(in_filename_part)
+        this_dict['out_filename'] = '{0}_ACCESS_{1}.nc'.format(out_filename_part, 
+                                                               yearmonth)
+        this_dict['in_filepath'] = os.path.join(base_dir, yearmonth + '/')
+        this_dict['out_filepath'] = os.path.join(base_dir, 'monthly', yearmonth + '/')
+        this_dict['interpolate'] = 'True'
+        this_dict['site_name'] = site_name
+        
+        master_dict['Sites'][dict_name] = this_dict
+    
+    ### Build the 'Variables' control dict
+    for key in access_alias_dict:
+        master_dict['Variables'][key] = {'access_name': access_alias_dict[key]}
+    
+    ### Build the options dict
+    master_dict['Options'] = {'WriteExcelIntermediate' : 'No',
+                              'WriteExcelNoGaps': 'No'}
+    
+    return master_dict
+
 # !!! end of function definitions !!!
 
 # !!! start of main program !!!
@@ -538,20 +623,25 @@ formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', '%H:%M:%S
 console.setFormatter(formatter)
 console.setLevel(logging.INFO)
 logging.getLogger('').addHandler(console)
+
+# Initialisations
+base_dir = '/rdsi/market/access_opendap'
+
 # get the control file name from the command line
 #cf_name = sys.argv[1]
 cf_name = qcio.get_controlfilename(path='../controlfiles',title='Choose a control file')
 # get the control file contents
 logging.info('Reading the control file')
-cf = configobj.ConfigObj(cf_name)
+#cf = configobj.ConfigObj(cf_name)
+cf = makecontroldict(
 # get stuff from the control file
 logging.info('Getting control file contents')
 site_list = cf["Sites"].keys()
 var_list = cf["Variables"].keys()
 # loop over sites
 #site_list = ["AdelaideRiver"]
-pdb.set_trace()
 for site in site_list:
+    pdb.set_trace()
     info = get_info_dict(cf,site)
     logging.info("Processing site "+info["site_name"])
     # instance the data structures
