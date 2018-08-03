1a072797ec9cc250641537bfbbedce9294425f7a
Ian McHugh <ian_mchugh@fastmail.com>
2018-08-03 15:10:03 +1000

    To be combined with new_ACCESS to form new processing script

--- file_list.py
+++ file_list.py
@@ -83,28 +83,30 @@ def get_access_names_from_dates(dates):
 #------------------------------------------------------------------------------
 
 #------------------------------------------------------------------------------
-def get_date_site_lists(site_file_path, data_file_path, fmt_date_list):
+def get_date_site_lists(site_file_path, data_file_path, server_file_list):
     """Get a list of sites that require data for each available date and time
        on server"""
 
     sites_df = get_ozflux_site_list(site_file_path)
-    bool_df = pd.DataFrame(index = fmt_date_list)
+    bool_df = pd.DataFrame(index = server_file_list)
     for site in sites_df.index:
         name = '_'.join(site.split(' '))
         f_name = '{}_ACCESS.csv'.format(name)
-        if f_name in os.listdir(data_file_path):
-            df = pd.read_csv(os.path.join(data_file_path, f_name), usecols = [0])
-            dates = map(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'), 
-                        df.date_time)
-            utc_dates = convert_utc(dates, 
-                                    lat = sites_df.loc[site, 'Latitude'],
-                                    lon = sites_df.loc[site, 'Longitude'],
-                                    direction = 'to_utc')
-            mod_dates = filter(lambda x: np.mod(x.hour, 6) == 0, utc_dates)
-            str_dates = map(lambda x: dt.datetime.strftime(x, '%Y%m%d%H'), 
-                            mod_dates)
-            site_fmt_date_list = get_access_names_from_dates(str_dates)
-            bool_df[name] = map(lambda x: x in site_fmt_date_list, fmt_date_list)
+        if not f_name in os.listdir(data_file_path): 
+            bool_df[name] = np.tile(np.nan, len(server_file_list))
+            continue
+        df = pd.read_csv(os.path.join(data_file_path, f_name), usecols = [0])
+        dates = map(lambda x: dt.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'), 
+                    df.date_time)
+        utc_dates = convert_utc(dates, 
+                                lat = sites_df.loc[site, 'Latitude'],
+                                lon = sites_df.loc[site, 'Longitude'],
+                                direction = 'to_utc')
+        mod_dates = filter(lambda x: np.mod(x.hour, 6) == 0, utc_dates)
+        str_dates = map(lambda x: dt.datetime.strftime(x, '%Y%m%d%H'), 
+                        mod_dates)
+        site_fmt_date_list = get_access_names_from_dates(str_dates)
+        bool_df[name] = map(lambda x: x in site_fmt_date_list, server_file_list)
     d = {}
     bool_df = bool_df.T
     for col in bool_df.columns:
@@ -125,8 +127,8 @@ site_file_path = '/home/ian/Temp/site_master.xls'
 # Make list of time_date file names available on the opendap server
 dir_list = list_opendap_dirs(url)
 date_list = map(lambda x: str(x.split('/')[-2]), dir_list)
-fmt_date_list = get_access_names_from_dates(date_list)
+server_file_list = get_access_names_from_dates(date_list)
 
 # Make list of time_date file names already written to existing file
-list_dict = get_date_site_lists(site_file_path, data_file_path, fmt_date_list)
+list_dict = get_date_site_lists(site_file_path, data_file_path, server_file_list)
 
